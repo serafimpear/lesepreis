bb778ae98210a4943669dc34673b5f6e230b7b77
Serafim Thaler <serafim.winnie.pooh@gmail.com>
2023-08-28 00:38:39 +0300

    titleBar

--- dist/assets/Manage-03175504.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-8386dab3.js";const n={};function r(a,c){return"manage"}const _=e(n,[["render",r]]);export{_ as default};
---
+++ dist/assets/Manage-1cbc5945.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-5c12ecc6.js";const n={};function r(a,c){return"manage"}const _=e(n,[["render",r]]);export{_ as default};
