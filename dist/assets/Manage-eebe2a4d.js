d40aa0e8ef35215e1dce56f1c17767dd30adab6e
Serafim Thaler <serafim.winnie.pooh@gmail.com>
2023-09-01 17:20:48 +0300

    dist update

--- dist/assets/Manage-eebe2a4d.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-cbee64fc.js";const n={};function r(a,c){return"manage"}const _=e(n,[["render",r]]);export{_ as default};
--- dist/assets/Settings-c2c11eb1.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./index-cbee64fc.js";const e={};function n(r,s){return"settings"}const _=t(e,[["render",n]]);export{_ as default};
