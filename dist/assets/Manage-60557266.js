aaa4d4ab6a42f9b687af7c09a05e6167e269af39
Serafim Thaler <serafim.winnie.pooh@gmail.com>
2023-09-02 17:56:21 +0300

    remove 'dist'

--- dist/assets/Manage-60557266.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:dist/assets/Manage-34e12037.js
-import{_ as e}from"./index-79ba41b7.js";const n={};function r(a,c){return"manage"}const _=e(n,[["render",r]]);export{_ as default};
-========
-import{_ as e}from"./index-aac10586.js";const n={};function r(a,c){return"manage"}const _=e(n,[["render",r]]);export{_ as default};
->>>>>>>> 3891079a17a13c03b04650275a2524aafd937074:dist/assets/Manage-60557266.js
--- dist/assets/Settings-8d5bd780.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:dist/assets/Settings-6986ff24.js
-import{_ as t}from"./index-79ba41b7.js";const e={};function n(r,s){return"settings"}const _=t(e,[["render",n]]);export{_ as default};
-========
-import{_ as t}from"./index-aac10586.js";const e={};function n(r,s){return"settings"}const _=t(e,[["render",n]]);export{_ as default};
->>>>>>>> 3891079a17a13c03b04650275a2524aafd937074:dist/assets/Settings-8d5bd780.js
