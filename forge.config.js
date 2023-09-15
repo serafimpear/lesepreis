d87f908dfddd4537aa854b4ae1344b8937ae320d
Serafim Thaler <serafim.winnie.pooh@gmail.com>
2023-09-15 22:03:23 +0200

    forge

--- forge.config.js
+++ forge.config.js
@@ -1,16 +1,20 @@
+const path = require('path');
+
 module.exports = {
-    "name": "Lesepreis Verwaltung",
-    "author": "Serafim Thaler, David Mairhofer",
-    "description": "Eine App, basiert auf vue.js und electron.js für die Verwaltung vom Lesepreis in den Oberschulen Fallmerayer",
-    "exe": `lesepreis-verwaltung.exe`,
-    "iconUrl": "https://raw.githubusercontent.com/serafimpear/lesepreis/SQLite-Test/lesepreis-logo.ico",
-    "owners": "serafim thaler",
-    "setupExe": `lesepreis-verwaltung-installer.exe`,
-    "setupIcon": "lesepreis-logo.ico",
-    "loadingGif": "loading-animation.gif",
+    name: "Lesepreis Verwaltung",
+    author: "Serafim Thaler, David Mairhofer",
+    description: "Eine App, basiert auf vue.js und electron.js für die Verwaltung vom Lesepreis in den Oberschulen Fallmerayer",
+    exe: `lesepreis-verwaltung.exe`,
+    iconUrl: "https://raw.githubusercontent.com/serafimpear/lesepreis/SQLite-Test/lesepreis-logo.ico",
+    owners: "serafim thaler",
+    setupExe: `lesepreis-verwaltung-installer.exe`,
+    setupIcon: "lesepreis-logo.ico",
+    loadingGif: "loading-animation.gif",
     packagerConfig: {
         asar: true,
-        "ignore": [ "src", "vite.config.js", "README.md", "index.html", ".gitignore", ".git" ]
+    },
+    electronPackagerConfig: {
+        icon: path.resolve(__dirname, 'lesepreis-logo.ico')
     },
     rebuildConfig: {},
     makers: [
