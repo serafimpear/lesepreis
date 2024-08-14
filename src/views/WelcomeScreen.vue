1bbecc6dcabe628076c67b1c787724ad80b9ef0a
Serafim Thaler <serafim.winnie.pooh@gmail.com>
2024-08-14 13:45:06 +0200

    multiple changes

--- src/views/WelcomeScreen.vue
+++ src/views/WelcomeScreen.vue
@@ -27,13 +27,30 @@
                         </div>
                     </div>
                 </div>
-                <Button class="add-year" tabindex="2" type="add" text="Schuljahr hinzuf&uuml;gen" @click="newYear()" />
+                <div class="hinweise">
+                    <p><b>Hinweise:</b></p>
+                    <div class="hinweis">
+                        <img src="@/assets/svgs/edit.svg" class="table-icon"><span>Zum Berabeiten vom Namen</span>
+                        <img src="@/assets/svgs/trashcan.svg" class="table-icon"><span>Zum Löschen</span>
+                        <img src="@/assets/svgs/export.svg" class="table-icon"><span>Zum Exportieren</span>
+                    </div>
+                </div>
+                <div class="button-box">
+                    <Button class="import-year" tabindex="3" type="import" text="Jahr importieren"
+                        @click="importYear()" />
+                    <Button class="add-year" tabindex="2" type="add" text="Jahr erstellen" @click="newYear()" />
+                </div>
             </div>
         </div>
+        <div class="bottom-text"><b>UI, Logik:</b> Serafim Thaler, 5AN&nbsp;&nbsp;&nbsp;<b>Logik, Datenbanken:</b> David
+            Mairhofer, 5AT&nbsp;&nbsp;&nbsp;<b>Sortierung-Algorithmus:</b> Elias Walter Ebner, 5AN</div>
         <div class="bottom-line"></div>
     </div>
     <Modal v-show="modalVisible" :title="modalTitle" :subtitle="modalSubtitle" :textCancel="modalButtonTextCancel"
         :textOK="modalButtonTextOK" @close="handleModalClose" :type="modalType"> {{ modalContent }} </Modal>
+    <TextInputWindow v-model="modelValue" text="Nachname" v-show="textinputwindowVisible" :title="textinputwindowTitle"
+        :textCancel="textinputwindowButtonTextCancel" :textOK="textinputwindowButtonTextOK"
+        @close="handleTextinputwindowClose"> {{ textinputwindowContent }} </TextInputWindow>
 </template>
 
 <script>
@@ -41,14 +58,16 @@ const { ipcRenderer } = require('electron')
 const fs = require('fs');
 import Button from '@/components/Button.vue'
 import Modal from "@/components/Modal.vue"
+import TextInputWindow from "@/components/TextInputWindow.vue"
 import { modalFunctions } from "@/logic/modal.js"
+import { textinputFunctions } from "@/logic/textinputwindow.js"
 const isDev = require('electron-is-dev')
 
 export default {
-    mixins: [modalFunctions],
+    mixins: [modalFunctions, textinputFunctions],
 
     components: {
-        Button, Modal
+        Button, Modal, TextInputWindow
     },
 
     data() {
@@ -56,7 +75,8 @@ export default {
             isDev: isDev,
             statistics: {},
             years: ipcRenderer.sendSync("getListOfSchoolYears"),
-            version: process.env.VITE_APP_VERSION
+            version: process.env.VITE_APP_VERSION,
+            modelValue: ''
         }
     },
 
@@ -85,22 +105,54 @@ export default {
         },
 
         newYear: function () {
-            let name = 'NEUER NAME';
-            // temporary solution, should be replaced with a modal window
-            // text should be filtered for special characters and max length
+            this.guiTextInput({
+                type: 'input',
+                content: `Geben Sie den Namen für das neue Schuljahr ein:`,
+                okButton: 'Hinzufügen'
+            }, () => {
+                // code if entered string
+                console.log('Got: ' + this.modelValue);
+            }, () => {
+                // code if abort
+            });
+
+
+            // this.modelValue is already filtered for special characters and max length
+            // code to change year name:
+            //...
+
+        },
+
+        importYear: function () {
+            // open file and save it to the database files folder
+            // create a importFile function (similar to saveFile)
+            // ...
         },
 
         editYearName: function (year) {
-            let name = 'NEUER NAME';
-            // temporary solution, should be replaced with a modal window
-            // text should be filtered for special characters and max length
+            this.guiTextInput({
+                type: 'input',
+                content: `Geben Sie den neuen Namen für das Schuljahr “${year}” ein:`,
+                okButton: 'Name ändern'
+            }, () => {
+                // code if entered string
+                console.log('Got: ' + this.modelValue);
+            }, () => {
+                // code if abort
+            });
+
+
+            // this.modelValue is already filtered for special characters and max length
+            // code to change year name:
+            //...
+
         },
 
         deleteYear: function (year) {
             this.ask({
                 type: 'warning',
                 subtitle: 'Schuljahr löschen',
-                content: `Sind Sie sicher, dass sie den Schuljahr “${year}” löschen wollen?     ACHTUNG DIESE AKTION KANN NICHT RÜCKGÄNGIG GEMACHT WERDEN!`,
+                content: `Sind Sie sicher, dass sie das Schuljahr “${year}” löschen wollen?     ACHTUNG DIESE AKTION KANN NICHT RÜCKGÄNGIG GEMACHT WERDEN!`,
                 okButton: 'Löschen',
             }, () => {
                 // code to delete year
@@ -161,8 +213,13 @@ div.welcome-screen-instance {
 .welcome-controls-box {
     display: flex;
     align-items: center;
-    height: 50%;
+    height: 60%;
     margin-bottom: 1em;
+}
+
+.button-box {
+    display: flex;
+    gap: 1em;
 }
 
 img.welcome-controls-box-background-logo {
@@ -179,24 +236,26 @@ img.welcome-controls-box-logo {
     box-shadow: rgb(0 0 0 / 10%) 0 0 138px;
     border-radius: 12px;
     padding: 50px;
-    max-width: 433px;
+    max-width: 500px;
     z-index: 1;
     transform: translate(-21%, 0);
     height: 100%;
     display: grid;
-    grid-template-rows: 1fr 40px;
+    grid-template-rows: 1fr auto 40px;
 }
 
 div.ui-table.years-table {
     margin: 1em 0;
     height: unset;
+    height: 22vh;
+    overflow-y: auto;
 }
 
 div.ui-table.years-table .table-row {
     grid-template-columns: 1fr 21px 21px 21px;
 }
 
-.add-year {
+.import-year {
     margin-left: auto;
 }
 
@@ -209,5 +268,42 @@ div.ui-table.years-table .table-row {
     .welcome-controls {
         transform: translate(-15%);
     }
+
+    .welcome-controls-box {
+        margin-bottom: 3em;
+    }
+
+    .hinweise {
+        display: none;
+    }
+
+    div.ui-table.years-table {
+        height: 26vh;
+    }
+
+    html body div.years-table img.table-icon {
+        height: 1.1em;
+        vertical-align: middle;
+    }
+
+    .bottom-text {
+        transform: scale(0.9);
+    }
+
+    .welcome-controls-box {
+        height: 57%;
+    }
+}
+
+.bottom-text {
+    position: absolute;
+    bottom: 4em;
+}
+
+.hinweis {
+    display: grid;
+    grid-template-columns: 30px 1fr;
+    margin-bottom: 1em;
+    gap: 2px 0;
 }
 </style>
