6f7ededa8c50c6a8584c68ee2881e8f44d6594b3
Serafim Thaler <serafim.winnie.pooh@gmail.com>
2024-09-11 16:29:36 +0200

    add api key infobox

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -2,7 +2,12 @@
     <main>
         <div class="manage-section">
             <h1>Einstellungen</h1>
-            <InputField v-model="googleAPIKey" text="Google API Key"
+            <div class="ui-infobox">
+                <div style="font-size: 16px;"><b>Achtung:</b>
+                    sollte das Programm die Bücher nach dem ISBN-Code nicht automatisch finden, muss höchstwahrscheinlich der Google API Key erneuert oder ersetzt werden. Bitte wenden Sie sich an einen Informatik-Lehrer, um den API Key zu erneuern.
+                </div>
+            </div>
+            <InputField class="apikeyinput" v-model="googleAPIKey" text="Google API Key"
                         :value="googleAPIKey" />
             <Button style="margin-left:auto;margin-top:10px" type="yes" text="Speichern" color="green" @click="setAPIKey()" />
         </div>
@@ -67,5 +72,13 @@ export default {
 .export-inline-icon {
     height: 1.8em;
     vertical-align: -0.5em;
+}
+
+.apikeyinput {
+    margin: 1em 0;
+}
+
+.ui-infobox div {
+    white-space: pre-wrap;
 }
 </style>
